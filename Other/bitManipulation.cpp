97f72c6d0ae19f07d074c7956ca48eb054553172
DrakeCullen <dpcullen@mavs.coloradomesa.edu>
2022-02-16 11:50:57 -0700

    Create new folder

---
+++ Other/bitManipulation.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+#define LSOne(S) ((S) & -(S))  
+#define isOn(S, j) (S & (1<<j))
+#define setBit(S, j) (S |= (1<<j))
+#define clearBit(S, j) (S &= ~(1<<j))
+#define toggleBit(S, j) (S ^= (1<<j))
+#define lowBit(S) (S & (-S))
+#define setAll(S, n) (S = (1<<n)-1)
+#define modulo(S, N) ((S) & (N-1))   // returns S % N, where N is a power of 2
+#define isPowerOfTwo(S) (!(S & (S-1)))
+#define nearestPowerOfTwo(S) (1<<lround(log2(S)))
+#define turnOffLastBit(S) ((S) & (S-1))
+#define turnOnLastZero(S) ((S) | (S+1))
+#define turnOffLastConsecutiveBits(S) ((S) & (S+1))
+#define turnOnLastConsecutiveZeroes(S) ((S) | (S-1))
+
+int cntSetBits(int n) {
+    int cnt=0;
+    while(n>0) {
+        cnt++;
+        n = n & (n - 1);
+    }
+    return cnt;
+}
+
+int main() {
+  ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+  cout<<cntSetBits(11);
+  return 0;
+}
